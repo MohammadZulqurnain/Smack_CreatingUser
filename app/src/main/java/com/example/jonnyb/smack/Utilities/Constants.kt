5cf2ed96940e3d584cfec961d77c8620c4266d2d
Mohammad Zulqurnain <prodigiousguy@hotmail.com>
2018-06-04 22:13:34 +0500

    Added the add user functionality

---
+++ app/src/main/java/com/example/jonnyb/smack/Utilities/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.jonnyb.smack.Utilities
+
+/**
+ * Created by jonnyb on 9/1/17.
+ */
+
+const val BASE_URL = "https://smackchatappbinaryleaf.herokuapp.com/v1/"
+//const val BASE_URL = "http://localhost:3005/chat-api/v1/"
+const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
